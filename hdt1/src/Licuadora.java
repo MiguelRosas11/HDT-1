b961fb5ed46988ac6a7c3391c666427454df2dd8
Diego Quan <dquan@delicatessen502.com>
2025-01-20 12:22:06 -0600

    Interfaz

--- hdt1/src/Licuadora.java
+++ hdt1/src/Licuadora.java
@@ -1,2 +1,10 @@
 public interface Licuadora {
+    void encender();
+    void apagar();
+    void llenar (int peso);
+    void vaciar();
+    void subirVelocidad();
+    void bajarVelocidad();
+    int consultarVelocidad();
+    boolean llena();
 }
