6073c268e48860925854780e38bb59890f73556d
Gonza <gon24170@uvg.edu.gt>
2025-01-22 08:20:23 -0600

    implementacion de los primeros 3 metodos en Main: crearLicuadora, encender y llenarLicuadora.

--- hdt1/src/Main.java
+++ hdt1/src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        ModelLicuadora model = new ModelLicuadora();
+
+        //Creacion de la Licuadora.
+        model.crearLicuadora(2.0, 5);
+
+        //Encender la licuadora.
+        model.encender();
+        System.out.println("La licuadora esta encendida: " + model.estaEncendida());
+
+        //llena la licaudora.
+        double llenarVolumen = 1.0;
+        if(model.verificarLLenado(llenarVolumen)){
+            model.llenarVolumen(llenarVolumen);
+            System.out.println("Licuadora llena: " + llenarVolumen + "litros.");
+        } else {
+            System.out.println("La licuadora no se puede llenar con: " + llenarVolumen + "litros");
+        }
     }
 }
