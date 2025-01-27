c8238a6fed2d4192e1de390db567dd082490af75
Gonza <gon24170@uvg.edu.gt>
2025-01-27 12:02:24 -0600

    Comentarios para ModelLicuadora

--- hdt1/src/ModelLicuadora.java
+++ hdt1/src/ModelLicuadora.java
@@ -10,7 +10,7 @@ public class ModelLicuadora implements Licuadora { //Implementa la interfaz licu
         licuadora = new LicuadorClass(capacidadTotal, velocidadMax);
     }
 
-    public boolean verificarLLenado(double llenarVolumen){ //Este método
+    public boolean verificarLLenado(double llenarVolumen){ //Este método verifica que el volumen que se quiere meter a la licuadora no exceda la capacidad maxima
         if(llenarVolumen > licuadora.getCapacidadActual() || llenarVolumen< 0){
             return false;
         } 
@@ -18,14 +18,14 @@ public class ModelLicuadora implements Licuadora { //Implementa la interfaz licu
     }
 
     public boolean verificarVelocidad(){
-        if(licuadora.getVelocidadActual()>=licuadora.getVelocidadMax()){
+        if(licuadora.getVelocidadActual()>=licuadora.getVelocidadMax()){ //Este metodo verifica que la velocidad que se quiere en la licuadora no exceda la velocidad maxima de la misma
             return false;
         }
 
         return true;
     }
 
-    public boolean verificarBajada(){
+    public boolean verificarBajada(){ //Este metodo verifica que se pueda seguir bajando la velocidad
         if(licuadora.getVelocidadActual() == 0){
             return false;
         }
@@ -33,7 +33,7 @@ public class ModelLicuadora implements Licuadora { //Implementa la interfaz licu
         return true;
     }
 
-    public boolean verificarServido(double servir){
+    public boolean verificarServido(double servir){ //Este metodo calcula cuando volumen se esta sirviendo
         if ((licuadora.getCapacidadTotal()-licuadora.getCapacidadActual())<= servir || servir < 0){
             return false;
         }
@@ -42,17 +42,17 @@ public class ModelLicuadora implements Licuadora { //Implementa la interfaz licu
     }
 
     @Override
-    public void encender(){
+    public void encender(){ //Este metodo enciende la licuadora esta encendida o apagada
         licuadora.setEstado(true);
     }
 
     @Override
-    public void apagar(){
+    public void apagar(){ //Este metodo apaga la licuadora
         licuadora.setEstado(false);
     }
 
     @Override
-    public boolean estaEncendida(){
+    public boolean estaEncendida(){ //Este metodo muestra si la licuadora esta encendida o apagada
         if (licuadora.getEstado()){
             return true;
         } 
